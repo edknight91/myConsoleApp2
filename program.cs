acf01008ab3d0406dbc717afa4a80522cc5d08fa
edknight91 <ed.knight1991@gmail.com>
2021-10-21 15:28:02 +0100

    Added the WriteAllTimesTable method

--- program.cs
+++ program.cs
@@ -4,39 +4,85 @@ namespace myConsoleApp2
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            int index, timesBy;
+            // int index;
             bool shouldRepeat = true;
 
             while (shouldRepeat)
             {
                 Console.WriteLine("Enter the number of the multiplication table you'd like to view");
-                timesBy = Convert.ToInt32(Console.ReadLine());
+                string firstInput = (Console.ReadLine());
 
-                for (index = 0; index <= 12; index++)
+                //Check the user has entered a number
+
+                int timesBy;
+                bool isNumber = int.TryParse(firstInput, out timesBy);
+
+                //If user has entered a number we run this
+
+                if (isNumber)
                 {
-                    Console.WriteLine($"{index} X {timesBy} = {index * timesBy} \n");
+                    WriteTimesTables(timesBy);
+
+                    Console.WriteLine("Press Y for another times table, press A to print all or press any other key to quit");
+
+                    var secondInput = Console.ReadKey().Key;
+
+                    switch (secondInput)
+                    {
+                        case ConsoleKey.Y:
+                            Console.WriteLine($"\n");
+                            break;
+
+                        case ConsoleKey.A:
+                            Program.WriteAllTimesTables();
+                            break;
+
+                        default:
+                            shouldRepeat = false;
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            Console.WriteLine("See ya skip");
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            break;
+                    }
                 }
 
-                Console.WriteLine("Press Y for another times table or any other key to quit");
-
-                var input = Console.ReadKey().Key;
-
-                switch (input)
+                // Quits program if the user didn't enter a number in the first instance
+                else
                 {
-                    case ConsoleKey.Y:
-                        Console.WriteLine($"\n");
-                        break;
-                    default:
-                        shouldRepeat = false;
-                        Console.WriteLine();
-                        Console.WriteLine();
-                        Console.WriteLine("See ya skip");
-                        Console.WriteLine();
-                        Console.WriteLine();
-                        break;
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("That's not a number you donut, start this thing again!");
+                    shouldRepeat = false;
                 }
+            }
+        }
+
+
+
+        // Method runs WriteTimesTables 12 times if user selects all
+        static void WriteAllTimesTables()
+
+        {
+            for (int index = 1; index <= 12; index++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"This is the {index} times table");
+                Console.WriteLine();
+                WriteTimesTables(index);
+            }
+        }
+
+        // Method prints the selected multplication table to the console
+        public static void WriteTimesTables(int timesBy)
+        {
+            for (int index = 1; index <= 12; index++)
+            {
+                Console.WriteLine($"{index} X {timesBy} = {index * timesBy} \n");
             }
         }
     }
